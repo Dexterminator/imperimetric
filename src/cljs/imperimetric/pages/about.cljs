18ec244a794915587bf61a7d26e997c15d2d2c6c
Dexter Gramfors <dexter.gramfors@gmail.com>
2016-08-09 17:27:51 +0200

    Added more info to about page.

--- src/cljs/imperimetric/pages/about.cljs
+++ src/cljs/imperimetric/pages/about.cljs
@@ -1,19 +1,47 @@
 (ns imperimetric.pages.about)
 
-(defn about-panel []
-  (fn [] [about]))
+(defn string-ul [& items]
+  [:ul
+   (for [item items]
+     ^{:key item} [:li item])])
 
-(defn about []
+(defn description []
   [:div
-   (str "Today, the metric system is used in a majority of the world's countries. "
-        "However, US customary units and the Imperial system still prevail, and people from countries "
-        "that use different systems often encounter recipes in systems they don't know by heart. "
-        "Remembering how to convert between systems of measurements is tedious. "
-        "Instead of you having to convert from cups to deciliters or the other way around, ")
-   [:span#app-name-text "imperimetric"]
-   (str " aims to convert the whole text automatically. For example, \"3 cups milk "
-        "and 2 tablespoons sugar\" becomes \"7.1 dl milk and 29.6 ml sugar\".")
-   [:h5 "More info:"]
+   [:h2 "Description"]
+   [:p
+    (str "Today, the metric system is used in a majority of the world's countries. "
+         "However, US customary units and the Imperial system still prevail, and people from countries "
+         "that use different systems often encounter recipes in systems they don't know by heart. ")]
+   [:p
+    (str "Remembering how to convert between systems of measurements is tedious. "
+         "Instead of you having to convert from cups to deciliters or the other way around, ")
+    [:span#app-name-text "imperimetric"]
+    (str " aims to convert the whole text automatically. For example, \"3 cups milk "
+         "and 2 tablespoons sugar\" becomes \"7.1 dl milk and 29.6 ml sugar\".")]])
+
+(defn supported-units []
+  [:div
+   [:h2 "Supported units"]
+   [:h3.supported-unit-system "Metric"]
+   [:div.supported-units
+    [:div
+     [:h4.supported-unit-type "Volume"]
+     [string-ul "Liters" "Deciliters" "Centiliters" "Milliliters"]]
+    [:div
+     [:h4.supported-unit-type "Distance"]
+     [string-ul "Kilometers" "Meters" "Decimeters" "Millimeters"]]]
+   [:h3.supported-unit-system "US and Imperial"]
+   [:div.supported-units
+    [:div
+     [:h4.supported-unit-type "Volume"]
+     [string-ul "Cups" "Ounces" "Tablespoons" "Teaspoons" "Gallons" "Pints" "Quarts"]]
+    [:div
+     [:h4.supported-unit-type "Distance"]
+     [string-ul "Miles" "Yards" "Feet" "Inches"]]]])
+
+(defn more-info []
+  [:div
+   [:h2 "More info"]
    [:ul
     [:li [:a {:href "https://en.wikipedia.org/wiki/System_of_measurement" :target "_blank"}
           "System of measurement"]]
@@ -23,3 +51,12 @@
           "US customary units"]]
     [:li [:a {:href "https://en.wikipedia.org/wiki/Imperial_units" :target "_blank"}
           "Imperial units"]]]])
+
+(defn about []
+  [:div
+   [description]
+   [supported-units]
+   [more-info]])
+
+(defn about-panel []
+  (fn [] [about]))
