8195bcc49e30702a2bc836c5a905d006012e5ade
Dexter Gramfors <dexter.gramfors@gmail.com>
2016-08-10 20:10:35 +0200

    Improved base grammar with brackets for symbols where we only care about children.

--- src/clj/imperimetric/convert.clj
+++ src/clj/imperimetric/convert.clj
@@ -122,12 +122,11 @@
 
 (defn transform-map [from-system to-system]
   (merge
-    (map-all-to [:text :token :word :whitespace] str)
     (map-all-to [:integer :fraction :decimal] read-string)
-    (map-all-to [:quantity :numeral :20-99] identity)
     (map-all-to [:1-9 :10-19 :base] (comp numeral->int str/lower-case))
     (map-all-to [:base-with-suffix :mixed] +)
-    {:measurement      (partial convert from-system to-system)
+    {:text             str
+     :measurement      (partial convert from-system to-system)
      :unicode-fraction unicode->fraction
      :unit             first}))
 
