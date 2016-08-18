3e7573d696752d1b4a94155721f65ad89bbd3423
Dexter Gramfors <dexter.gramfors@gmail.com>
2016-08-18 23:57:27 +0200

    Removed 'more info' from about and have links in motivation text instead.

--- src/cljs/imperimetric/pages/about.cljs
+++ src/cljs/imperimetric/pages/about.cljs
@@ -1,23 +1,26 @@
 (ns imperimetric.pages.about)
 
-(defn string-ul [items]
-  [:ul
-   (for [item items]
-     ^{:key item} [:li item])])
-
 (defn motivation []
   [:div
    [:h2 "Motivation"]
    [:p
-    (str "Today, the metric system is used in a majority of the world's countries. "
-         "However, US customary units and the Imperial system still prevail, and people from countries "
-         "that use different systems often encounter recipes and other text in systems they don't know by heart. ")]
+    "Today, the "
+    [:a {:href "https://en.wikipedia.org/wiki/Metric_system" :target "_blank"} "metric system"]
+    " is used in a majority of the world's countries. "
+    "However, "
+    [:a {:href "https://en.wikipedia.org/wiki/United_States_customary_units" :target "_blank"} "US customary units"]
+     " and the "
+    [:a {:href "https://en.wikipedia.org/wiki/Imperial_units" :target "_blank"} "Imperial system"]
+     " still prevail, and people from countries "
+    "that use different systems often encounter recipes and other text in systems they don't know by heart. "]
    [:p
-    (str "Remembering how to convert between systems of measurements is tedious. "
-         "Instead of you having to convert from cups to deciliters or the other way around, ")
+     "Remembering how to convert between "
+    [:a {:href "https://en.wikipedia.org/wiki/System_of_measurement" :target "_blank"} "systems of measurement"]
+     " is tedious. "
+    "Instead of you having to convert from cups to deciliters or the other way around, "
     [:span#app-name-text "imperimetric"]
-    (str " aims to convert the whole text automatically. For example, \"3 cups milk "
-         "and 2 tablespoons sugar\" becomes \"7.1 dl milk and 29.6 ml sugar\".")]])
+    (str " aims to convert the whole text automatically. For example, \"2 cups milk "
+         "and 4 tablespoons sugar\" becomes \"4.73 dl milk and 59.1 ml sugar\".")]])
 
 (defn unit-list-div [title units]
   [:div.unit-list
@@ -32,45 +35,33 @@
    [:h3.supported-unit-system "Metric"]
    [:div.supported-units
     [unit-list-div "Volume" [{:unit "Liters" :symbols "(liters, litres, l)"}
-                              {:unit "Deciliters" :symbols "(deciliters, decilitres, dl)"}
-                              {:unit "Centiliters" :symbols "(centiliters, centilitres, cl)"}
-                              {:unit "Milliliters" :symbols "(milliliters, millilitres, ml)"}]]
+                             {:unit "Deciliters" :symbols "(deciliters, decilitres, dl)"}
+                             {:unit "Centiliters" :symbols "(centiliters, centilitres, cl)"}
+                             {:unit "Milliliters" :symbols "(milliliters, millilitres, ml)"}]]
     [unit-list-div "Distance" [{:unit "Kilometers" :symbols "(kilometers, kilometres, km)"}
-                                {:unit "Meters" :symbols "(meters, metres, m)"}
-                                {:unit "Decimeters" :symbols "(decimeters, decimetres, dm)"}
-                                {:unit "Centimeters" :symbols "(centimeters, centimetres, cm)"}
-                                {:unit "Millimeters" :symbols "(millimeters, millimetres, mm)"}]]
+                               {:unit "Meters" :symbols "(meters, metres, m)"}
+                               {:unit "Decimeters" :symbols "(decimeters, decimetres, dm)"}
+                               {:unit "Centimeters" :symbols "(centimeters, centimetres, cm)"}
+                               {:unit "Millimeters" :symbols "(millimeters, millimetres, mm)"}]]
     [unit-list-div "Weight" [{:unit "Kilograms" :symbols "(kilograms, kg)"}
-                              {:unit "Hectograms" :symbols "(hectograms, hg)"}
-                              {:unit "Grams" :symbols "(grams, g)"}
-                              {:unit "Milligrams" :symbols "(milligrams, mg)"}]]]
+                             {:unit "Hectograms" :symbols "(hectograms, hg)"}
+                             {:unit "Grams" :symbols "(grams, g)"}
+                             {:unit "Milligrams" :symbols "(milligrams, mg)"}]]]
    [:h3.supported-unit-system "US and Imperial"]
    [:div.supported-units
     [unit-list-div "Volume" [{:unit "Cups" :symbols "(cups, cp)"}
-                              {:unit "Fluid ounces" :symbols "(fluid ounces, fl. oz, floz)"}
-                              {:unit "Tablespoon" :symbols "(tablespoons, tbsp)"}
-                              {:unit "Teaspoon" :symbols "(teaspoons, tsp)"}
-                              {:unit "Gallons" :symbols "(gallons, gal)"}
-                              {:unit "Pints" :symbols "(pints, pt)"}
-                              {:unit "Quarts" :symbols "(quarts, qt)"}]]
+                             {:unit "Fluid ounces" :symbols "(fluid ounces, fl. oz, floz)"}
+                             {:unit "Tablespoon" :symbols "(tablespoons, tbsp)"}
+                             {:unit "Teaspoon" :symbols "(teaspoons, tsp)"}
+                             {:unit "Gallons" :symbols "(gallons, gal)"}
+                             {:unit "Pints" :symbols "(pints, pt)"}
+                             {:unit "Quarts" :symbols "(quarts, qt)"}]]
     [unit-list-div "Distance" [{:unit "Miles" :symbols "(miles, mi)"}
-                                {:unit "Yards" :symbols "(yards, yd)"}
-                                {:unit "Feet" :symbols "(foot, feet, ft, ')"}
-                                {:unit "Inches" :symbols "(inches, in, \")"}]]
+                               {:unit "Yards" :symbols "(yards, yd)"}
+                               {:unit "Feet" :symbols "(foot, feet, ft, ')"}
+                               {:unit "Inches" :symbols "(inches, in, \")"}]]
     [unit-list-div "Weight" [{:unit "Pounds" :symbols "(pounds, lb)"}
-                              {:unit "Ounces" :symbols "(ounces, oz)"}]]]])
-
-(defn more-info []
-  [:div
-   [:h2 "More info"]
-   [:div [:a {:href "https://en.wikipedia.org/wiki/System_of_measurement" :target "_blank"}
-          "System of measurement"]]
-   [:div [:a {:href "https://en.wikipedia.org/wiki/Metric_system" :target "_blank"}
-          "Metric system"]]
-   [:div [:a {:href "https://en.wikipedia.org/wiki/United_States_customary_units" :target "_blank"}
-          "US customary units"]]
-   [:div [:a {:href "https://en.wikipedia.org/wiki/Imperial_units" :target "_blank"}
-          "Imperial units"]]])
+                             {:unit "Ounces" :symbols "(ounces, oz)"}]]]])
 
 (defn faq []
   [:div
@@ -88,8 +79,7 @@
   [:div
    [motivation]
    [supported-units]
-   [faq]
-   [more-info]])
+   [faq]])
 
 (defn about-panel []
   (fn [] [about]))
