f30588857d17b8d83d6b5574bfe6bd45e4342fa9
Dexter Gramfors <dexter.gramfors@gmail.com>
2016-08-18 20:17:09 +0200

    Don't throw on very large ratios.

--- src/clj/imperimetric/convert.clj
+++ src/clj/imperimetric/convert.clj
@@ -42,7 +42,7 @@
   ((parsers from-system) text))
 
 (defn significant-digits [n]
-  (let [stripped (.stripTrailingZeros (bigdec n))
+  (let [stripped (.stripTrailingZeros (with-precision 100 (bigdec n)))
         precision (.precision stripped)
         scale (.scale stripped)]
     (if (< scale 0)
