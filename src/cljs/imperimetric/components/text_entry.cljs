8e19002ca36e0ab09b2197f264807114878a213b
Dexter Gramfors <dexter.gramfors@gmail.com>
2016-09-01 08:25:02 +0200

    New placeholder.

--- src/cljs/imperimetric/components/text_entry.cljs
+++ src/cljs/imperimetric/components/text_entry.cljs
@@ -2,10 +2,14 @@
   (:require [re-frame.core :refer [dispatch subscribe]]
             [imperimetric.utils-js :refer [target-value]]))
 
+(def placeholder
+  (str "Paste or write some text here, like \"3 cups of milk and 1/4 pound butter\" or \"My 450 sq ft apartment is 2 miles"
+  " away\", or \"the engine temperature exceeded 250°F as the car reached 110 mph\"."))
+
 (defn text-entry []
   (let [text (subscribe [:text])]
     (fn []
       [:textarea#text-entry {:max-length  "2800"
-                             :placeholder "Paste or write some text here, like '3 cups of milk and 2 tablespoons of sugar.'"
+                             :placeholder placeholder
                              :on-change   #(dispatch [:text-changed (target-value %)])
                              :value       @text}])))
