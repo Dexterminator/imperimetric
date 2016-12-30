90dbfde59ac9e4721dbeb5ed3baebc620623dcb9
Dexter Gramfors <dexter.gramfors@gmail.com>
2016-12-30 23:41:14 +0100

    Use logging instead of throwing in check spec interceptor for clearer display of values and specs in console.

--- src/cljs/imperimetric/handlers.cljs
+++ src/cljs/imperimetric/handlers.cljs
@@ -1,6 +1,6 @@
 (ns imperimetric.handlers
-  (:require [re-frame.core :refer [reg-event-db reg-event-fx reg-fx reg-cofx
-                                   inject-cofx trim-v dispatch debug after]]
+  (:require [re-frame.core :refer [reg-event-db reg-event-fx reg-fx reg-cofx get-effect get-coeffect
+                                   inject-cofx trim-v dispatch debug after ->interceptor]]
             [imperimetric.db :as db]
             [imperimetric.api :as api]
             [clojure.string :as str]
@@ -89,12 +89,28 @@
                            :latest-requested-text changed-text
                            :text changed-text)})))
 
-(defn check-and-throw
-  [a-spec db]
-  (when-not (s/valid? a-spec db)
-    (throw (ex-info (str "spec check failed: " (s/explain-str a-spec db)) {}))))
+(defn check-spec
+  [a-spec db event]
+  (when-not (nil? db)
+    (when-let [problems (::s/problems (s/explain-data a-spec db))]
+      (js/console.group "Spec errors after event: " event)
+      (doseq [{:keys [pred val via in path]} problems]
+        (js/console.error "Spec check failed")
+        (js/console.log "val: " val)
+        (js/console.log "in: " in)
+        (js/console.log "failed spec: " (last via))
+        (js/console.log "on predicate: " pred)
+        (js/console.log "path: " path))
+      (js/console.groupEnd))))
 
-(def check-spec-interceptor (after (partial check-and-throw :imperimetric.db/db)))
+(def check-spec-interceptor
+  (->interceptor
+    :id :check-spec
+    :after (fn check-spec-after [context]
+             (let [db    (get-effect context :db)
+                   event (get-coeffect context :event)]
+               (check-spec ::imperimetric.db/db db event)
+               context))))
 
 (def standard-interceptors [(when config/debug? [debug check-spec-interceptor]) trim-v])
 
